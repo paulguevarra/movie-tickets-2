bf8537630bb4eeff7ad3333760b20a485a464f2f
Bart Webber and Paul Guevarra <bart@>
2017-07-18 14:02:06 -0700

    create movie object constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 //Business Logic
+function Movie(name, premium, showtime){
+  this.name =  name;
+  this.premium = premium;
+  this.showtime = showtime;
+}
+
+var wonder = new Movie("Wonder Woman", false, [8,11,17,20]);
+var dunkirk = new Movie("Dunkirk", true, [8,11,17,20]);
+var spiderman = new Movie("Spiderman: Homecoming", true, [8,11,17,20]);
+var dispicable = new Movie("Dispicable ME:3", false, [8,11,17,20]);
+
+
+
 function toCost (x, y, z){
 
   var userMovie = "";
